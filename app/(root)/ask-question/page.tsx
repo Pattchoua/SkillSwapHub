a089e5968f03f1a29204032440c7686330d1122a
Patrick Tchoua <tchouapatrick@gmail.com>
2023-10-12 18:54:05 +0200

    mongoUser updated

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,12 +1,12 @@
 import Question from "@/components/forms/Question";
-//import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import React from "react";
 import { getUserById } from "@/lib/actions/user.actions";
 
 const Page = async () => {
-  //const { userId } = auth();
-  const userId = "1234567890";
+  const { userId } = auth();
+
 
   if (!userId) redirect("/sign-in");
   const mongoUser = await getUserById({ userId });
@@ -15,7 +15,8 @@ const Page = async () => {
     <div>
       <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
       <div className="mt-9">
-        <Question mongoUserId={JSON.stringify(mongoUser._id)} />
+      <Question mongoUserId={JSON.stringify(mongoUser?._id)} />
+
       </div>
     </div>
   );
